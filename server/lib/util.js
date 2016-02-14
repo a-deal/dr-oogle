9b9a218ee3674a2becbbb88f9dc2cd06307bd89a
Andrew Deal <deal.e.andrew@gmail.com>
2016-02-14 14:27:14 -0800

    (feat) Adds Review and Dentist utility functions

--- server/lib/util.js
+++ server/lib/util.js
@@ -1,20 +1,39 @@
+import Xray from 'x-ray';
+// import Osmosis from 'osmosis';
 
-/**	Creates a callback that proxies node callback style arguments to an Express Response object.
- *	@param {express.Response} res	Express HTTP Response
- *	@param {number} [status=200]	Status code to send on success
- *
- *	@example
- *		list(req, res) {
- *			collection.find({}, toRes(res));
- *		}
- */
-export function toRes(res, status=200) {
-	return (err, thing) => {
-		if (err) return res.status(500).send(err);
+export function findAndParseReviews (url) {
+	return new Promise((resolve, reject) => {
+		let x  = Xray();
+		x(url, '#copyreviews', [{
+			'text' : '@text'
+		}])
+		.paginate('#copyheade11:last-child@href')
+		.limit(10)
+		((err, results) => {
+			if (err) {
+				reject(err)
+			} else {
+				let comments = results.filter((review) => {
+						review.text = review.text.replace(/\s\s+/g, ' ');
+						return review.text.length > 35 && review.text.match(/Review/);
+				})
+				resolve(parseReviews(comments))
+			}
+		})
+	})
+}
 
-		if (thing && typeof thing.toObject==='function') {
-			thing = thing.toObject();
+function parseReviews (reviews) {
+	let name = reviews[0].text.match(/Dr.(.*?):/)[1].trim();
+	reviews = reviews.map((review) => {
+	 let dateRegex = /[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])/;
+	 let comment = {
+		  date   : review.text.match(dateRegex)[0],
+		  author : review.text.match(/by\s(.*?)[0-9]/)[1].trim(),
+		  rating : review.text.match(/\s[[0-9].[0-9]\sstars|\s[0-9]\sstars/)[0].match(/[0-9].[0-9]|([0-9]{1})/)[0],
+			comment : review.text.slice((review.text.search(dateRegex)+11))
 		}
-		res.status(status).json(thing);
-	};
+		return comment;
+	})
+	return { name : name, reviews : reviews };
 }
